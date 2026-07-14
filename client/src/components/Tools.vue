3f7a285dadb41bb75deb7438c77e6d5ef6bdea71
sebastianangeles14 <sebastianangeles14@gmail.com>
2026-07-14 13:30:24 +0800

    fix tablet size about section

--- client/src/components/Tools.vue
+++ client/src/components/Tools.vue
@@ -2,136 +2,145 @@
   <!-- Tools Section Start -->
   <section id="tools" class="bg-black">
     <div class="container text-center">
-      <h1 class="mb-5 pt-5 display-4 fw-medium">Developer Tools</h1>
+      <h1 class="mb-5 pt-5 display-4 fw-medium text-white">Developer Tools</h1>
       <div class="row g-5 align-items-start">
 
-        <!-- Front-End Development -->
+        <!-- 1. Front-End Development -->
         <div class="col-lg-4 rounded-5 d-flex flex-column pb-5">
-          <h3 class="mb-5 p-3">Front-End Development</h3>
+          <h3 class="mb-5 p-3 text-white">Front-End Development</h3>
           <div class="row gx-2 gy-3 mx-auto">
-            <div class="col-4">
+            <div class="col-4 col-md-3 pb-5">
               <div class="tool-box">
                 <img src="https://cdn-icons-png.flaticon.com/512/1051/1051277.png" alt="HTML5" class="tech-icon">
               </div>
-              <p class="small mt-2">HTML5</p>
             </div>
-            <div class="col-4">
+            <div class="col-4 col-md-3 pb-5">
               <div class="tool-box">
                 <img src="https://cdn-icons-png.flaticon.com/512/732/732190.png" alt="CSS3" class="tech-icon">
               </div>
-              <p class="small mt-2">CSS3</p>
             </div>
-            <div class="col-4">
+            <div class="col-4 col-md-3 pb-5">
               <div class="tool-box">
                 <img src="https://www.drupal.org/files/project-images/bootstrap-stack.png" alt="Bootstrap" class="tech-icon">
               </div>
-              <p class="small mt-2">Bootstrap</p>
             </div>
-            <div class="col-4">
+            <div class="col-4 col-md-3 pb-5">
               <div class="tool-box">
                 <img src="https://cdn-icons-png.flaticon.com/512/5968/5968292.png" alt="JavaScript" class="tech-icon">
               </div>
-              <p class="small mt-2">JavaScript</p>
             </div>
-            <div class="col-4">
+            <div class="col-4 col-md-3 pb-5">
               <div class="tool-box">
                 <img src="https://icon.icepanel.io/Technology/svg/Vue.js.svg" alt="Vue.js" class="tech-icon">
               </div>
-              <p class="small mt-2">Vue.js</p>
             </div>
-          </div>
-        </div>
-
-        <!-- Back-End Development -->
-        <div class="col-lg-4 rounded-5 d-flex flex-column pb-5">
-          <h3 class="mb-5 p-3">Back-End Development</h3>
-          <div class="row gx-2 gy-3 mx-auto">
-            <div class="col-4">
-              <div class="tool-box">
-                <img src="https://icon.icepanel.io/Technology/svg/MongoDB.svg" alt="MongoDB" class="tech-icon">
-              </div>
-              <p class="small mt-2">MongoDB</p>
-            </div>
-            
-            <div class="col-4">
-              <div class="tool-box">
-                <img src="https://img.icons8.com/?size=160&id=9Gfx4Dfxl0JK&format=png" alt="Express.js" class="tech-icon">
-              </div>
-              <p class="small mt-2">Express.js</p>
-            </div>
-
-            <div class="col-4">
+            <div class="col-4 col-md-3 pb-5">
               <div class="tool-box">
                 <img src="https://icon.icepanel.io/Technology/svg/React.svg" alt="React.js" class="tech-icon">
               </div>
-              <p class="small mt-2">React.js</p>
             </div>
+          </div>
+        </div>
 
-            <div class="col-4">
+        <!-- 2. Back-End & Database -->
+        <div class="col-lg-4 rounded-5 d-flex flex-column pb-5">
+          <h3 class="mb-5 p-3 text-white">Back-End & Database</h3>
+          <div class="row gx-2 gy-3 mx-auto">
+            <div class="col-4 col-md-4 pb-5">
               <div class="tool-box">
                 <img src="https://icon.icepanel.io/Technology/svg/Node.js.svg" alt="Node.js" class="tech-icon">
               </div>
-              <p class="small mt-2">Node.js</p>
             </div>
-            
+            <div class="col-4 col-md-4 pb-5">
+              <div class="tool-box">
+                <img src="https://img.icons8.com/?size=160&id=9Gfx4Dfxl0JK&format=png" alt="Express.js" class="tech-icon">
+              </div>
+            </div>
+            <div class="col-4 col-md-4 pb-5">
+              <div class="tool-box">
+                <img src="https://icon.icepanel.io/Technology/svg/MongoDB.svg" alt="MongoDB" class="tech-icon">
+              </div>
+            </div>
           </div>
         </div>
 
-        <!-- Full-Stack Development -->
+        <!-- 3. Development Workflow & APIs -->
         <div class="col-lg-4 rounded-5 d-flex flex-column pb-5">
-          <h3 class="mb-5 p-3">Full-Stack Development</h3>
+          <h3 class="mb-5 p-3 text-white">Dev Workflow & Testing</h3>
           <div class="row gx-2 gy-3 mx-auto">
-            
-            <div class="col-4">
+            <div class="col-4 col-md-4 pb-5">
               <div class="tool-box">
                 <img src="https://cdn-icons-png.flaticon.com/512/15466/15466163.png" alt="Git" class="tech-icon">
               </div>
-              <p class="small mt-2">Git</p>
             </div>
-
-            <div class="col-4">
+            <div class="col-4 col-md-4 pb-5">
               <div class="tool-box">
                 <img src="https://img.icons8.com/?size=100&id=EPbEfEa7o8CB&format=png" alt="Postman" class="tech-icon">
               </div>
-              <p class="small mt-2">Postman</p>
             </div>
-
-          </div>
-        </div>
-
-        <!-- Design & Tools -->
-        <div class="col-lg-4 rounded-5 d-flex flex-column pb-5">
-          <h3 class="mb-5 p-3">Design & Tools</h3>
-          <div class="row gx-2 gy-3 mx-auto">
-            <div class="col-4">
-              <div class="tool-box">
-                <img src="https://cdn-icons-png.flaticon.com/512/5968/5968705.png" alt="Figma" class="tech-icon">
-              </div>
-              <p class="small mt-2">Figma</p>
-            </div>
-            <div class="col-4">
+            <div class="col-4 col-md-4 pb-5">
               <div class="tool-box">
                 <img src="https://img.icons8.com/?size=100&id=9OGIyU8hrxW5&format=png" alt="VS Code" class="tech-icon">
               </div>
-              <p class="small mt-2">VS Code</p>
             </div>
-            <div class="col-4">
+            <div class="col-4 col-md-4 pb-5">
               <div class="tool-box">
-                <img src="https://img.icons8.com/?size=100&id=dljSTzHrskUM&format=png&color=000000" alt="Sublime" class="tech-icon">
+                <img src="https://img.icons8.com/?size=100&id=dljSTzHrskUM&format=png&color=000000" alt="Sublime Text" class="tech-icon">
               </div>
-              <p class="small mt-2">Sublime</p>
             </div>
-            <div class="col-4">
+          </div>
+        </div>
+
+        <!-- 4. Design & Productivity -->
+        <div class="col-lg-6 rounded-5 d-flex flex-column pb-5">
+          <h3 class="mb-5 p-3 text-white">Design & Productivity</h3>
+          <div class="row gx-2 gy-3 mx-auto">
+            <div class="col-4 col-md-3 pb-5">
+              <div class="tool-box">
+                <img src="https://cdn-icons-png.flaticon.com/512/5968/5968705.png" alt="Figma" class="tech-icon">
+              </div>
+            </div>
+            <div class="col-4 col-md-3 pb-5">
+              <div class="tool-box">
+                <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/adobe-illustrator-icon.png" alt="Adobe Illustrator" class="tech-icon">
+              </div>
+            </div>
+            <div class="col-4 col-md-3 pb-5">
+              <div class="tool-box">
+                <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/adobe-photoshop-icon.png" alt="Adobe Photoshop" class="tech-icon">
+              </div>
+            </div>
+            <div class="col-4 col-md-3 pb-5">
               <div class="tool-box">
                 <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/trello-logo-icon.png" alt="Trello" class="tech-icon">
               </div>
-              <p class="small mt-2">Trello</p>
             </div>
-            <div class="col-4">
+            <div class="col-4 col-md-3 pb-5">
               <div class="tool-box">
                 <img src="https://cdn-icons-png.flaticon.com/128/888/888867.png" alt="Microsoft Tools" class="tech-icon">
               </div>
-              <p class="small mt-2">MS Tools</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- 5. AI & Dev Assistants -->
+        <div class="col-lg-6 rounded-5 d-flex flex-column pb-5">
+          <h3 class="mb-5 p-3 text-white">AI & Dev Assistants</h3>
+          <div class="row gx-2 gy-3 mx-auto">
+            <div class="col-4 col-md-4 pb-5">
+              <div class="tool-box">
+                <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/chatgpt-icon.png" alt="ChatGPT" class="tech-icon">
+              </div>
+            </div>
+            <div class="col-4 col-md-4 pb-5">
+              <div class="tool-box">
+                <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/claude-ai-icon.png" alt="Claude AI" class="tech-icon">
+              </div>
+            </div>
+            <div class="col-4 col-md-4 pb-5">
+              <div class="tool-box">
+                <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/cursor-ai-code-icon.png" alt="Cursor AI" class="tech-icon">
+              </div>
             </div>
           </div>
         </div>
